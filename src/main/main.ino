c362a024a682ce40d561df3e8a118b270b0858ee
Unknown <willmartens91@gmail.com>
2017-05-16 01:07:18 -0400

    Experiments with image loading
    Kris's face now loads with text scrolling on it. SmartMatrix has poor documentation

---
+++ src/main/main.ino
@@ -0,0 +1,87 @@
+/*
+    SmartMatrix Features Demo - Louis Beaudoin (Pixelmatix)
+    This example code is released into the public domain
+*/
+
+#include <SmartMatrix3.h>
+#include "kris.c"
+#include "gimpbitmap.h"
+
+#define COLOR_DEPTH 24                  // known working: 24, 48 - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24
+const uint8_t kMatrixWidth = 32;        // known working: 32, 64, 96, 128
+const uint8_t kMatrixHeight = 32;       // known working: 16, 32, 48, 64
+const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48
+const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
+const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
+const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
+const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
+const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
+const uint8_t kIndexedLayerOptions = (SM_INDEXED_OPTIONS_NONE);
+
+SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
+SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
+SMARTMATRIX_ALLOCATE_INDEXED_LAYER(indexedLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kIndexedLayerOptions);
+
+const int defaultBrightness = 100*(255/100);    // full brightness
+//const int defaultBrightness = 15*(255/100);    // dim: 15% brightness
+const int defaultScrollOffset = 6;
+const rgb24 defaultBackgroundColor = {0x40, 0, 0};
+
+// Teensy 3.0 has the LED on pin 13
+const int ledPin = 13;
+
+void drawBitmap(int16_t x, int16_t y, const gimp32x32bitmap* bitmap) {
+  for(unsigned int i=0; i < bitmap->height; i++) {
+    for(unsigned int j=0; j < bitmap->width; j++) {
+      rgb24 pixel = { bitmap->pixel_data[(i*bitmap->width + j)*3 + 0],
+                      bitmap->pixel_data[(i*bitmap->width + j)*3 + 1],
+                      bitmap->pixel_data[(i*bitmap->width + j)*3 + 2] };
+
+      backgroundLayer.drawPixel(x + j, y + i, pixel);
+    }
+  }
+}
+
+// the setup() method runs once, when the sketch starts
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(38400);
+
+  matrix.addLayer(&backgroundLayer); 
+  matrix.addLayer(&scrollingLayer); 
+  matrix.addLayer(&indexedLayer); 
+  matrix.begin();
+
+  matrix.setBrightness(defaultBrightness);
+
+  scrollingLayer.setOffsetFromTop(defaultScrollOffset);
+
+  backgroundLayer.enableColorCorrection(true);
+}
+// the loop() method runs over and over again,
+// as long as the board has power
+void loop() {
+    int i, j;
+    unsigned long currentMillis;
+
+    // clear screen
+    backgroundLayer.fillScreen(defaultBackgroundColor);
+    backgroundLayer.swapBuffers();
+
+    // "SmartMatrix Demo"
+    scrollingLayer.setColor({0xff, 0xff, 0xff});
+    scrollingLayer.setMode(wrapForward);
+    scrollingLayer.setSpeed(40);
+    scrollingLayer.setFont(font6x10);
+    scrollingLayer.start("SmartMatrix3 has piss poor documentation", 1);
+
+    //backgroundLayer.fillScreen({0,0,0});
+    drawBitmap(0,0,&kris);
+    backgroundLayer.swapBuffers();
+    
+    delay(10000);
+
+}
